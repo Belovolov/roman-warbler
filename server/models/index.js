3a445ea3d55bde7eb47b350ec926e4933295d007
Roman Belovolov <romanchitto@gmail.com>
2018-10-04 13:15:12 +1300

    Preparing for continuous deployment with Docker and Travis CI

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 mongoose.set('debug', true)
-mongoose.connect('mongodb://localhost/warbler-db', {
-  keepAlive: true,
-  reconnectTries: Number.MAX_VALUE
-})
+
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds121593.mlab.com:21593/heroku_8kt2szcm`, 
+  {
+    keepAlive: true,
+    reconnectTries: Number.MAX_VALUE
+  })
     .then(()=>console.log("db connection successfull"))
     .catch(error=>console.log("db connection not successfull: " + error));
 
